55f4e621c4f9896482b3461a90641d67dd713ac0
praveen singh <praveensingh@gmail.com>
2022-09-17 05:44:48 -0500

    package change

--- src/main/demo/aws/simple/lambda/MainHandler.java
+++ src/main/demo/aws/simple/lambda/MainHandler.java
@@ -1,10 +1,10 @@
-package demo.lambda;
+package demo.aws.simple.lambda;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
-public class HandlerWeatherData implements RequestHandler<WeatherData, WeatherData> {
+public class MainHandler implements RequestHandler<WeatherData, WeatherData> {
 
     @Override
     public WeatherData handleRequest(WeatherData event, Context context) {
